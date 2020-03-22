374f2e711ef178957a85a2f3ea3e92b9e45c1992
0yykk <921122271@qq.com>
2020-03-22 15:26:05 +0800

    Add project files.

---
+++ PrinterySystem/Controllers/SystemOpController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrinterySystem.Controllers
+{
+    public class SystemOpController : Controller
+    {
+        // GET: SystemOp
+        public ActionResult Dashborad()
+        {
+            return View();
+        }
+    }
+}
---
+++ PrinterySystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrinterySystem.MvcApplication" Language="C#" %>
